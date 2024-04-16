6cb9e4e2f86f3e1529629d222e360a9cfaf5bfff
Ryan Hinkle <2176012+rhinkle@users.noreply.github.com>
2024-04-15 23:46:09 -0400

    feat: have totals dynamic.

--- src/app/tip-window/_tip-button/tip-button.tsx
+++ src/app/tip-window/_tip-button/tip-button.tsx
@@ -1,13 +1,13 @@
 export interface TipButtonProps {
   percent: number;
-  amount: number;
+  amount: string;
 }
 
 export default function TipButton({ percent, amount }: TipButtonProps) {
   return (
     <button className="w-1/3 bg-blue-500 p-5">
       {`${percent}%`}
-      <span className="block text-xs">${amount}</span>
+      <span className="block text-xs">{amount}</span>
     </button>
   );
 }
--- src/app/tip-window/page.tsx
+++ src/app/tip-window/page.tsx
@@ -1,17 +1,35 @@
 import TipButton from "./_tip-button/tip-button";
 
 export default function TipWindow() {
+  let USDollar = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  const total = 21.82;
+  const tipOptions: number[] = [10, 15, 20];
+
+  const tipOptionList = () => {
+    return tipOptions.map((tip) => {
+      const tipAmount = total * (tip / 100);
+      return (
+        <TipButton
+          key={tip}
+          amount={USDollar.format(tipAmount)}
+          percent={tip}
+        />
+      );
+    });
+  };
+
   return (
     <div className="flex h-full justify-center">
       <div className="flex h-full w-4/5 flex-col justify-center gap-3 align-middle lg:w-1/2">
         <div className="flex flex-col gap-2 text-center">
-          <div className="text-5xl">$1.07</div>
-          <div className="text-xl">Add Optional Tip</div>
+          <div className="text-5xl">${total}</div>
+          <div className="text-xl">Add optional tip</div>
         </div>
         <div className="flex justify-center gap-2 text-center text-white">
-          <TipButton percent={10} amount={0} />
-          <TipButton percent={20} amount={0} />
-          <TipButton percent={25} amount={0} />
+          {tipOptions && tipOptionList()}
         </div>
         <div className="flex flex-col gap-2">
           <button className="border-2 border-black">Custom Tip Ammount</button>
